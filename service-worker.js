611bae19c1a6d7b19ecfb6850ab3df4d79ddc68d
beichenzhidi <13520648600@qq.com>
2021-03-28 16:17:11 +0000

    Deploying to gh-pages from  @ 8306755364b37d18b6350f1971ba5024849cf79b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4da514158c1d7e63fc917eb296df5495.js"
+  "/precache-manifest.4471c1a74d83d1a6a3b8e3f118d59419.js"
 );
 
 self.addEventListener('message', (event) => {
